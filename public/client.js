a447290c742438d54aa2d7614b767fe472d450a9
YuliaOkushko <juli.okushko@gmail.com>
2020-06-01 20:26:27 +0300

    Create preloding image

--- public/client.js
+++ public/client.js
@@ -1,19 +1,22 @@
 const listBttn = document.getElementById('showListBttn');
 
-listBttn.onclick = function () { //todo: es6 ()=>{}
-    fetch('http://localhost:3000/getList', { //todo: relative path
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    })
-        .then(response => response.json()) //response reading method
-        .then(allListItems => {
-            allListItems.map(oneListItem => {
-                let div = document.createElement('div');
-                div.innerHTML = oneListItem.name;
-                document.body.append(div);
-                console.log(oneListItem);
-            });
+const temporaryDiv = document.createElement('div');
+document.body.className = "preloaderGif";
+
+fetch('http://localhost:3000/getList', { //todo: relative path
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+    .then(response => response.json()) //response reading method
+    .then(allListItems => {
+        allListItems.map(oneListItem => {
+            document.body.className = "body";
+            let div = document.createElement('div');
+            div.className = "oneItem";
+            div.innerHTML = oneListItem.numb;
+            div.innerHTML += " " + oneListItem.name;
+            document.body.append(div);
         });
-}
+    });
